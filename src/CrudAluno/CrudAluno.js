dd112ef23cc19d35811cef09c73baa4a15c440a1
LuizHPDomingues2005 <81539018+LuizHPDomingues2005@users.noreply.github.com>
2022-05-27 09:36:25 -0300

    UpdatingCrud

--- src/CrudAluno/CrudAluno.js
+++ src/CrudAluno/CrudAluno.js
@@ -35,16 +35,18 @@ export default class CrudAluno extends Component {
     salvar() {
         const aluno = this.state.aluno;
         aluno.codCurso = Number(aluno.codCurso);
-        const metodo = 'post';
+        const metodo = aluno.id ? 'put' : 'post';
+        const url = aluno.id ? `${urlAPI}/${aluno.id}` : urlAPI;
+
         axios[metodo](urlAPI, aluno)
             .then(resp => {
                 const lista = this.getListaAtualizada(resp.data)
                 this.setState({ aluno: initialState.aluno, lista })
             })
     }
-    getListaAtualizada(aluno) {
+    getListaAtualizada(aluno, add = true) {
         const lista = this.state.lista.filter(a => a.id !== aluno.id);
-        lista.unshift(aluno);
+        if(add) lista.unshift(aluno);
         return lista;
     }
     atualizaCampo(event) {
@@ -55,6 +57,22 @@ export default class CrudAluno extends Component {
         //atualizar o state
         this.setState({ aluno });
     }
+    carregar(aluno) {
+        this.setState({ aluno })
+    }
+    remover(aluno) {
+        const url = urlAPI + "/" + aluno.id;
+        if (window.confirm("Confirma remoção do aluno: " + aluno.ra)) {
+            console.log("entrou no confirm");
+            axios['delete'](url, aluno)
+                .then(resp => {
+                    const lista = this.getListaAtualizada(aluno, false)
+                    this.setState({ aluno: initialState.aluno, lista })
+                })
+        }
+    }
+
+
     renderForm() {
         return (
             <div className="inclui-container">
@@ -125,6 +143,16 @@ export default class CrudAluno extends Component {
                                     <td>{aluno.ra}</td>
                                     <td>{aluno.nome}</td>
                                     <td>{aluno.codCurso}</td>
+                                    <td>
+                                        <button onClick={() => this.carregar(aluno)}>
+                                            Alterar
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button onClick={() => this.remover(aluno)}>
+                                            Remover
+                                        </button>
+                                    </td>
                                 </tr>
                         )}
                     </tbody>
